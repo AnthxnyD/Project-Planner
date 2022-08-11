f767d969cf7e6c9e65e949d13541b1684de3c6b6
Xavier Brigode <xavier.brigode@gmail.com>
2022-08-11 14:16:38 +0200

    card update

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,4 @@
 let card_data = [
-    {
-        name : "carglaz",
-        description : "cezadaz zdazdad",
-        timer : "466",
-        status : "1"
-    },
-    {
-        name : "olvier",
-        description : "cezadaz zdazdad",
-        timer : "466",
-        status : "1"
-    }
 ]
 let dashboard = document.querySelector(".dashboard")
 
@@ -20,10 +8,21 @@ function generateCard(){
     dashboard.innerHTML = ""
     card_data.forEach((card) => {
         let cardElem = document.createElement("div")
+        let cardName = document.createElement("h2")
+        let cardDescription = document.createElement("p")
+        let cardDate = document.createElement("p")
+
         cardElem.classList.add("card")
-        cardElem.innerHTML = "check test " + card.name
-        
+        // cardElem.innerHTML = "Titre " + card.name
+        cardName.innerHTML = card.name
+        cardDescription.innerHTML = card.description
+        cardDate.innerHTML = card.timer
+
+
         dashboard.appendChild(cardElem)
+        cardElem.appendChild(cardName)
+        cardElem.appendChild(cardDescription)
+        cardElem.appendChild(cardDate)
     })
 }
 
@@ -37,44 +36,51 @@ addCard.addEventListener("click", function() {
 })
 
 function displayCheckbox(){
-let planner = document.querySelector(".planner")
-let checkbox = document.createElement("section")
-let validation = document.createElement("button")
-let inputName = document.createElement("input")
-let test = inputName.innerHTML
+    let planner = document.querySelector(".planner") // Slection de l'element .planner
+    let checkbox = document.createElement("section") // Création de section
+    let validation = document.createElement("button") // Création d'un button
+    let inputName = document.createElement("input") // Création d'un input field
+    let inputDescription = document.createElement("input")
+    let inputDate = document.createElement("input")
+    
+    const name = document.createElement("div")
+    name.innerHTML = "Name :"
+    const description = document.createElement("div")
+    description.innerHTML = "Description :"
+    const date = document.createElement("div")
+    date.innerHTML = "Date d'échéance :"
+    
 
-checkbox.classList.add("checkbox")
-checkbox.innerHTML="Your name : "
-inputName.setAttribute("type","text")
 
-card_data.push({name: inputName.innerText})
 
-checkbox.appendChild(inputName)
+    checkbox.classList.add("checkbox") // Ajoute a checkbox
+    inputName.setAttribute("type","text") // Ajoute un attribut type a l'input field
+    inputDescription.setAttribute("type","text")
+    inputDate.setAttribute("type", "date")
+    validation.innerHTML = "valider" // Ajouter du texte au button
 
-validation.innerHTML = "valider"
-checkbox.appendChild(validation)
-
-planner.insertBefore(checkbox , dashboard)
-validation.addEventListener("click", function(){
-    generateCard()    
-    planner.removeChild(checkbox)
+    validation.addEventListener("click", function(){ // Ajoute au button un event on click
+        let newName = inputName.value
+        let newDescription = inputDescription.value
+        let newDate = inputDate.value
+        card_data.push({
+            name: newName,
+            description: newDescription,
+            timer: newDate
+        }) // Insère dans l'array un nouvelle objet
+        generateCard() // lance la fonction generate card
+        console.log(newDate)
+        planner.removeChild(checkbox) // supprime l'enfant checkbox
 })
+
+planner.insertBefore(checkbox , dashboard) // Ajoute dans le dom l'elem checkbox
+checkbox.appendChild(name)
+checkbox.appendChild(inputName) // Ajoute dans le dom l'elem inputName
+checkbox.appendChild(description)
+checkbox.appendChild(inputDescription)
+checkbox.appendChild(date)
+checkbox.appendChild(inputDate)
+checkbox.appendChild(validation) // Ajoute dans le dom l'elem validation
 
 }
 
-
-
-// let displayCard = card_data.map(function(){
-//         let card = document.createElement("div")
-//         card.classList.add("card")
-//         let dashboard = document.querySelector(".dashboard")
-//         dashboard.appendChild(card)
-// })
-// function displayCard (){
-//     console.log(e)
-//     let card = document.createElement("div")
-//     card.classList.add("card")
-//     card.textContent = ""
-//     let dashboard = document.querySelector(".dashboard")
-//     dashboard.appendChild(card)
-    
